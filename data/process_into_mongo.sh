b2c17fd31019eb1d6dfe77cc5e00993f661c3789
Ian Webster <ianw_github@ianww.com>
2016-06-11 22:48:41 -0700

    delete temp file

--- data/process_into_mongo.sh
+++ data/process_into_mongo.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cat publicschools_20132014.tsv | ./process.py > processed_output.csv
+pushd `dirname $0`
 
+cat publicschools_20132014.tsv | ./process.py > processed_output.csv
 mongoimport -d categories -c schools --type csv --file processed_output.csv --headerline
+rm processed_output.csv
+
+popd
