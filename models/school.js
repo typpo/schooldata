42a6a891e1fc7e3fb796d826b5ef208c425bee98
Ian Webster <ianw_github@ianww.com>
2016-12-12 23:58:11 -0800

    Get street address working

--- models/school.js
+++ models/school.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 var mongoosePaginate = require('mongoose-paginate');
+var slug = require('slug');
+
 var Schema = mongoose.Schema;
 
 var schoolSchema = new Schema({
@@ -20,7 +22,35 @@ var schoolSchema = new Schema({
   //'lng': Number,
 
   //'locale_type': String,
+
+  location_address_street_1: String,
+  location_address_street_2: String,
+  location_address_street_3: String,
 });
+
+// NOTE that these slug methods rely on node slug matching the beavhior of
+// python-slug.
+schoolSchema.methods.getSlug = function() {
+  return slug(this.name, '_');
+};
+
+schoolSchema.methods.getAgencySlug = function() {
+  return slug(this.agency, '_');
+};
+
+schoolSchema.methods.getStreetAddress = function() {
+  if (!this.location_address_street_1) {
+    return '';
+  }
+  var str = this.location_address_street_1;
+  if (this.location_address_street_2) {
+    str += ', ' + this.location_address_street_2;
+  }
+  if (this.location_address_street_3) {
+    str += ', ' + this.location_address_street_3;
+  }
+  return str;
+};
 
 schoolSchema.plugin(mongoosePaginate);
 var School = mongoose.model('School', schoolSchema);
