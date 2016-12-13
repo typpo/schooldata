95e9686ad54732b226b9d7d212f4ecc34bbeb2f8
Ian Webster <ianw_github@ianww.com>
2016-12-13 01:00:45 -0800

    add some data to school page

--- models/school.js
+++ models/school.js
@@ -26,6 +26,28 @@ var schoolSchema = new Schema({
   location_address_street_1: String,
   location_address_street_2: String,
   location_address_street_3: String,
+
+  all_students_american_indian_alaska_native: String,
+  all_students_american_indian_alaska_native_female: String,
+  all_students_american_indian_alaska_native_male: String,
+  all_students_asian: String,
+  all_students_asian_female: String,
+  all_students_asian_male: String,
+  all_students_black: String,
+  all_students_black_female: String,
+  all_students_black_male: String,
+  all_students_hawaiian_native_pacific_islander: String,
+  all_students_hawaiian_native_pacific_islander_female: String,
+  all_students_hawaiian_native_pacific_islander_male: String,
+  all_students_hispanic: String,
+  all_students_hispanic_female: String,
+  all_students_hispanic_male: String,
+  all_students_two_or_more_races: String,
+  all_students_two_or_more_races_female: String,
+  all_students_two_or_more_races_male: String,
+  all_students_white: String,
+  all_students_white_female: String,
+  all_students_white_male: String,
 });
 
 schoolSchema.methods.getStreetAddress = function() {
