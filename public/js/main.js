82173d32382f3d52a18dbe6e37ca56cf03121339
Ian Webster <ianw_github@ianww.com>
2016-12-25 14:42:47 -0800

    Add toggleable race table to district page. Also use local jquery.

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,9 @@
 $(function() {
+  $('.visibility-toggle').on('click', function() {
+    $($this.data('for')).toggle();
+    return false;
+  });
+
   $('.jquery-datatable').DataTable({
     paging: false,
     order: [[1, 'desc']],
