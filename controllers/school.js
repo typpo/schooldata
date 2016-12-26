77705b5236222c6d8781ca267399d4e5bd3869f5
Judy Mou <moujudy@gmail.com>
2016-12-25 17:56:38 -0800

    got embeded google map working

--- controllers/school.js
+++ controllers/school.js
@@ -1,7 +1,9 @@
 var School = require('../models/school');
 
-function getStaticMapUrl(lat, lng) {
-  return 'http://maps.googleapis.com/maps/api/staticmap?zoom=2&scale=1&size=350x250&maptype=terrain&format=png&visual_refresh=true&markers=size:mid%7Ccolor:red%7Clabel:1%7C' + lat + ',' + lng + '&key=' + process.env.MAPS_API_KEY;
+function getStaticMapUrl(streetAddress, city, state, zipcode) {
+  var address = streetAddress + '+' + city + '+' + state + '+' + zipcode;
+  address = address.replace(/ /g, '+');
+  return 'https://www.google.com/maps/embed/v1/place?q=' + address + '&key=' + process.env.MAPS_API_KEY;
 }
 
 function getStreetViewUrl(lat, lng) {
@@ -20,7 +22,7 @@ exports.index = function(req, res) {
       },
       school: school,
       map: {
-        url: getStaticMapUrl(school.lat, school.lng),
+        url: getStaticMapUrl(school.getStreetAddress(), school.city, school.state, school.zip),
       },
       streetview: {
         url: getStreetViewUrl(school.lat, school.lng),
